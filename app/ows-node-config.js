f04826ecf00b55d9141171ea42ec41ea04dbd22c
Ian Patton <ian@owstack.org>
2019-03-03 16:42:49 -0500

    remove generated config from build

--- app/ows-node-config.js
+++
@@ -1,9 +0,0 @@
-angular.module('owsExplorerApp').constant('nodeConfig', {
-  "routePrefix": "",
-  "fullNodes": [
-    {
-      "url": "https://dev.owstack.org",
-      "apiPrefix": "/api/explorer/ltc"
-    }
-  ]
-});
